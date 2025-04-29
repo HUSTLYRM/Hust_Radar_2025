a42288a8b4f10d6b64bbc9302e806d6aff1f897c
KIng Kevin <147916477+jinyankai@users.noreply.github.com>
2025-04-29 18:51:53 +0800

    Update start_ros.sh

--- start_ros.sh
+++ start_ros.sh
@@ -3,8 +3,9 @@
 # 启动第一个终端并运行 roscore
 gnome-terminal -- bash -c "roscore; exec bash"
 
+sleep 5
 # 启动第二个终端并运行 livox_ros_driver
 gnome-terminal -- bash -c "cd ~/catkin_ws/ws_livox && source devel/setup.sh && roslaunch livox_ros_driver livox_lidar.launch; exec bash"
-
+sleep 5
 # 启动第三个终端并运行 pclmatcher
 gnome-terminal -- bash -c "cd ~/RadarWorkspace/code/PCLMATCHER && source devel/setup.sh && roslaunch pclmatcher pclmatcher.launch; exec bash"
