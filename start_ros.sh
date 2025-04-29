7dff79efe71a37651886ef204fe225603ffe8e58
KIng Kevin <147916477+jinyankai@users.noreply.github.com>
2025-04-29 18:58:00 +0800

    Update start_ros.sh
    更新自启动脚本

--- start_ros.sh
+++ start_ros.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# 杀死已启动的 roscore 进程
+echo "Killing existing roscore processes..."
+pkill -f roscore
 
 # 启动第一个终端并运行 roscore
+echo "Starting roscore in terminal 1..."
 gnome-terminal -- bash -c "roscore; exec bash"
 
 sleep 5
+
 # 启动第二个终端并运行 livox_ros_driver
+echo "Starting livox_ros_driver in terminal 2..."
 gnome-terminal -- bash -c "cd ~/catkin_ws/ws_livox && source devel/setup.sh && roslaunch livox_ros_driver livox_lidar.launch; exec bash"
+
 sleep 5
+
 # 启动第三个终端并运行 pclmatcher
+echo "Starting pclmatcher in terminal 3..."
 gnome-terminal -- bash -c "cd ~/RadarWorkspace/code/PCLMATCHER && source devel/setup.sh && roslaunch pclmatcher pclmatcher.launch; exec bash"
